a534ee7fbab20a1a847cb78a0c22adc5e0dc94c4
raurodse <raurodse@gmail.com>
2017-10-04 21:25:19 +0200

    add package

--- app/settings.py
+++ app/settings.py
@@ -1,9 +1,9 @@
-RSRC = "rsrc/"
+RSRC = "/usr/share/noteshrink-gui/rsrc/"
 CSS_FILE = RSRC + "noteshrink-gui.css"
 UI_FILE = RSRC + "noteshrink.ui"
 WINDOW_TITLE = "Noteshrink"
 
-# IMAGES 
+# IMAGES
 DROP_FILE = RSRC+"rsrc/drop_file.png"
 DROP_CORRECT = RSRC+"rsrc/drop_file_correct.png"
 DROP_INCORRECT = RSRC+"rsrc/drop_file_incorrect.png"
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(noteshrink-gui):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="noteshrink-gui" command="/usr/bin/noteshrink-gui"
