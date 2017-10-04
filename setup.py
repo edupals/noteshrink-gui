0f9c5c638bf3e44937494f9e24217f879522ed9e
Raul Rodrigo Segura <raurodse@gmail.com>
2017-10-04 16:04:10 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import os
+import sys
+from setuptools import setup
+
+setup(
+    name="noteshrink-gui",
+    version="0.1.0",
+    author="Raul Rodrigo Segura",
+    description="Graphical user interface for noteshrink",
+    url="https://github.com/lliurex/noteshrink-gui.git",
+    py_modules=["noteshrinkgui"]
+)
