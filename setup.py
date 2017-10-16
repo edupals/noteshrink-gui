a98bc635200fd803d928ca360a9a46380b37c9e8
Raul Rodrigo Segura <raurodse@gmail.com>
2017-10-16 14:44:17 +0200

    add translations install function

--- setup.py
+++ setup.py
@@ -19,8 +19,41 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from distutils.core import setup
+from setuptools import setup
+from distutils.command.build import build
+import polib
 import os
+import shutil
+
+package = 'noteshrinkgui'
+po_folder = 'translations'
+temp_mo_folder = 'build/.mo_folder'
+dst_tmpl = "share/locale/{}/LC_MESSAGES/"
+mo_tmpl = "build/.mo_folder/{}/{}.mo"
+
+class Buildi18n(build):
+	def run(self,*args):
+		build.run(self,*args)
+		self.make_mo()
+	
+	def make_mo(self):
+		if os.path.exists(temp_mo_folder):
+			shutil.rmtree(temp_mo_folder)
+		os.makedirs(temp_mo_folder)
+		for x in lang_list():
+			aux_file = polib.pofile(os.path.join(po_folder,x + ".po"))
+			aux_path = os.path.join(temp_mo_folder, x)
+			os.makedirs(aux_path)
+			aux_file.save_as_mofile(os.path.join(aux_path, package + ".mo"))
+
+def lang_list():
+	return [ x[:-3] for x in os.listdir(po_folder) if x[-3:] == ".po" ]
+
+def polist():
+	polist = []
+	for lang in lang_list():
+		polist.append( (dst_tmpl.format(lang),[ mo_tmpl.format(lang,package) ] ))
+	return polist
 
 if __name__ == '__main__':
 
@@ -40,16 +73,16 @@ if __name__ == '__main__':
 		packages = ['noteshrinkgui'],
 		package_dir = {'noteshrinkgui':'app'},
 		package_data = {'noteshrinkgui':['rsrc/*']},
-		data_files = [('/usr/share/applications','noteshrink-gui.desktop'),('/usr/share/icons/hicolor/scalable/apps','NoteShrink.svg')]
+		data_files = [('share/applications',['noteshrink-gui.desktop']),('share/icons/hicolor/scalable/apps',['noteshrink.svg'])] + polist() ,
 		scripts = ['noteshrink-gui'],
 		classifiers=[
-				'Development Status :: 4 - Beta',
-				'Environment :: Console'
+				'Environment :: Console',
 				'Intended Audience :: End Users',
 				'License :: OSI Approved :: GNU General Public License v3',
 				'Operating System :: POSIX',
 				'Programming Language :: Python',
-				'Topic :: Software store',
-				'Topic :: Install apps',
 				],
+		cmdclass = {
+			'build':Buildi18n
+		}
 	)
